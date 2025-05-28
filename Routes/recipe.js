eefad327a24b3582912e5b41ad1fd88c4723a7c7
Robert Wagner <dwe@dwe.mshome.net>
2025-05-28 10:49:51 +0000

    AddRecipe  Route

--- Routes/recipe.js
+++ Routes/recipe.js
@@ -24,6 +24,27 @@ router.get('/recipe', async (req, res) => {
     }
 })
 
+router.post('/addRecipe', async (req, res) => {
+    const { name, preparation, ingredients } = req.body;
+    const userID = req.body.userID || "1"; 
+    console.log("userID:", req.body.userID);
+
+    if (!name || !preparation || !ingredients) {
+        return res.status(400).json({ error: 'Alle Felder werden benötigt' });
+    }
+
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        const result = await conn.query('INSERT INTO recipe (userID,recipeTitle, zubereitung, zutaten) VALUES (?, ?, ?, ?)', [userID,name, preparation, ingredients]);
+        res.status(201).json({ message: 'Rezept erfolgreich hinzugefügt' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Database error' });
+    } finally {
+        if (conn) conn.release();
+    }
+});
 
 
 export default router;
